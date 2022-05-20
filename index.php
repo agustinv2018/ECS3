6eba51591e2685095a25b8d81fd40e6f3e7e15e8
agustinv2018 <agustin.v420@gmail.com>
2022-05-19 22:37:30 -0300

    Nombre

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+//Evaluación conceptual de situación N 3
+//Crear un repositorio en GitHub llamado ECS3
+//Clonar el repositorio dentro del root del servidor PHP.
+//Agregar un archivo index.php y: 
+//Recibir una variable mediante post.
+//Nombre.
+//Devolver:
+//Nombre: [valorNombre]
+//Realizar el commit de los cambios, con el mensaje “Nombre”
+//Pushear los cambios al repositorio remoto.
+//En el mismo archivo index.php agregar:
+//Recibir otra variable mediante post
+//Apellido
+//Y la devolución debe quedar:
+//Nombre: [valorNombre]
+//Apellido: [valorApellido]
+//Realizar el commit de los cambios, con el mensaje “Se agrega apellido”
+//Pushear los cambios al repositorio remoto.
+
+echo 'Nombre : ' . $_POST['valornombre'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
